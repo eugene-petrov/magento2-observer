b2c42bd385951164f87462272c9ff3ff73ad2f30
Evgeny Petrov <evgeny_petrov@epam.com>
2021-09-29 12:51:18 +0300

    initial commit

---
+++ Observer/RedirectToContact.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Snippet\Observer\Observer;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\App\ActionFlag;
+use Magento\Framework\App\Response\RedirectInterface;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+
+class RedirectToContact implements ObserverInterface
+{
+    /**
+     * @var RedirectInterface
+     */
+    private $redirect;
+
+    /**
+     * @var ActionFlag
+     */
+    private $actionFlag;
+
+    /**
+     * @param RedirectInterface $redirect
+     * @param ActionFlag $actionFlag
+     */
+    public function __construct(RedirectInterface $redirect, ActionFlag $actionFlag)
+    {
+        $this->redirect = $redirect;
+        $this->actionFlag = $actionFlag;
+    }
+
+    /**
+     * @inheridoc
+     */
+    public function execute(Observer $observer)
+    {
+        $request = $observer->getEvent()->getData('request');
+        if ($request->getModuleName() === 'contact') {
+            return;
+        }
+
+        $controller = $observer->getControllerAction();
+        $this->actionFlag->set('', ActionInterface::FLAG_NO_DISPATCH, true);
+        $this->redirect->redirect($controller->getResponse(), 'contact');
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+***Snippet_Observer***
+
+_Example task:_ You need redirect all frontend pages to a contact form page
+
+To enable this module execute:
+- php bin/magento --clear-static-content module:enable Snippet_Observer
+- php bin/magento setup:upgrade
